f55acf1b4882a59462484178b3a39eba679101f5
catedrall <catedrall@gmail.com>
2019-02-02 19:19:28 -0200

    WebCore Funcional

--- RSBrasil.WebCore/Controllers/ClientesController.cs
+++ RSBrasil.WebCore/Controllers/ClientesController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RSBrasil.Model.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace RSBrasil.Web.Controllers
 {
     public class ClientesController : Controller
     {
+
+        private IConfiguration _configuration;
+
+        public ClientesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // GET: Clientes
         public ActionResult Index()
+        {
+            List<Cliente> clientes = GetClienteAsync();
+            return View(clientes);
+        }
+
+        public ActionResult Novo()
         {
             return View();
         }
@@ -13,6 +34,22 @@ namespace RSBrasil.Web.Controllers
         public ActionResult Edit()
         {
             return View();
+        }
+                
+        public List<Cliente> GetClienteAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = _configuration["EPListarClientes"];
+                var response = client.GetStringAsync(url);
+                var produtos = JsonConvert.DeserializeObject<List<Cliente>>(response.Result);
+                return produtos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
