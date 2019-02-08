5c6ce35f6f7a215f40853e2b5f5967c6b7026eb6
Ivo Ferreira Villa <ivofvilla@hotmail.com>
2019-02-08 15:59:18 -0200

    Adicionando telas Uniforme e Afastamento medico

---
+++ RSBrasil.WebCore/Controllers/AfastamentoMedicoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RSBRasil.Model.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RSBrasil.Web.Controllers
+{
+    public class AfastamentoMedicoController : Controller
+    {
+
+        private IConfiguration _configuration;
+
+        public AfastamentoMedicoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            List<Afastamentomedico> afastamento = GetClienteAsync();
+            return View(afastamento);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+                
+        public List<Afastamentomedico> GetClienteAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = _configuration["EPListarClientes"];
+                var response = client.GetStringAsync(url);
+                var afastamento = JsonConvert.DeserializeObject<List<Afastamentomedico>>(response.Result);
+                return afastamento;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //public Cliente Inserir(Form)
+    }
+}
---
+++ RSBrasil.WebCore/Controllers/UniformeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RSBrasil.Model.Entidades;
+using RSBRasil.Model.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RSBrasil.Web.Controllers
+{
+    public class UniformeController : Controller
+    {
+
+        private IConfiguration _configuration;
+
+        public UniformeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            List<Uniforme> afastamento = GetClienteAsync();
+            return View(afastamento);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+                
+        public List<Uniforme> GetClienteAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = _configuration["EPListarClientes"];
+                var response = client.GetStringAsync(url);
+                var uniforme = JsonConvert.DeserializeObject<List<Uniforme>>(response.Result);
+                return uniforme;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //public Cliente Inserir(Form)
+    }
+}
