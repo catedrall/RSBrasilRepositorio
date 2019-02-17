a1efd6bc31f6a778f677de5e8fb00cd71f525a03
Ivo Ferreira Villa <ivofvilla@hotmail.com>
2019-02-17 11:59:51 -0300

    tipo de documentos

---
+++ RSBrasil.Business/TipoDeDocumentosBusiness.cs
@@ -0,0 +1,82 @@
+﻿using RSBrasil.Data;
+using RSBrasil.Model.Entidades;
+using RSBrasil.Model.Interface.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSBrasil.Business
+{
+    public class TipoDeDocumentosBusiness
+    {
+        private IRepositorioBase<TipoDeDocumentos> repositorioTipoDeDocumentos = new Repositorio<TipoDeDocumentos>();
+
+        public TipoDeDocumentos Inserir(TipoDeDocumentos tipoDeDocumentos)
+        {
+            //var existe = repositorioUniforme.BuscaQualquerParametro(x => x.CNPJ == uniforme.CNPJ);
+            //if (existe == null)
+            //{
+                tipoDeDocumentos.DataInclusao = DateTime.Now;
+                return repositorioTipoDeDocumentos.Inserir(tipoDeDocumentos);
+            //}
+            //else
+            //{
+            //    return null;
+            //}
+        }
+
+        public TipoDeDocumentos BuscaDocumento(int Id)
+        {
+            if (Id > 0)
+            {
+                TipoDeDocumentos tipoDeDocumentos = repositorioTipoDeDocumentos.PesquisarPorId(Id);
+                if (tipoDeDocumentos != null)
+                {
+                    //uniforme.ColcarMascara();
+                }
+                return tipoDeDocumentos;
+            }
+            else
+                return null;
+        }
+
+        public List<TipoDeDocumentos> ListarTodos()
+        {
+            List<TipoDeDocumentos> tipoDeDocumentos = repositorioTipoDeDocumentos.Listar();
+            if (tipoDeDocumentos != null)
+            {
+                foreach (var item in tipoDeDocumentos)
+                {
+                    //item.ColcarMascara();
+                }
+            }
+            return tipoDeDocumentos;
+        }
+
+        public void ExcluirTipoDocumento(int Id)
+        {
+            if (Id > 0)
+            {
+                TipoDeDocumentos tipoDeDocumentos = repositorioTipoDeDocumentos.PesquisarPorId(Id);
+                repositorioTipoDeDocumentos.Excluir(tipoDeDocumentos);
+            }
+        }
+
+        /*public void EditarCliente(ClienteDTO uniforme)
+        {
+            if (uniforme != null)
+            {
+                uniforme local = repositorioUniforme.PesquisarPorId(uniforme.Id);
+                local.CNPJ = uniforme.CNPJ;
+                local.Contato = uniforme.Contato;
+                local.DataAlteracao = DateTime.Now;
+                local.Email = uniforme.Email;
+                local.IdContrato = uniforme.IdContrato;
+                local.NomeFantasia = uniforme.NomeFantasia;
+                local.RazaoSocial = uniforme.RazaoSocial;
+                local.Telefone = uniforme.Telefone;
+                repositorioUniforme.Atualizar(local);
+            }
+        }*/
+    }
+}
---
+++ RSBrasil.WebCore/Controllers/TipoDeDocumentosController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RSBrasil.Model.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace RSBrasil.Web.Controllers
+{
+    public class TipoDeDocumentosController : Controller
+    {
+
+        private IConfiguration _configuration;
+
+        public TipoDeDocumentosController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            List<TipoDeDocumentos> tipoDocumentos = GetClienteAsync();
+            return View(tipoDocumentos);
+        }
+
+        public ActionResult Novo()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+                
+        public List<TipoDeDocumentos> GetClienteAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = _configuration["EPListarClientes"];
+                var response = client.GetStringAsync(url);
+                var tipoDocumentos = JsonConvert.DeserializeObject<List<TipoDeDocumentos>>(response.Result);
+                return tipoDocumentos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //public Cliente Inserir(Form)
+    }
+}
